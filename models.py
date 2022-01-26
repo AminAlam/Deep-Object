e24d64f9236ef98a51961d372aa0e5d292736826
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-01-26 13:10:54 +0330

    Class Object detection implemetned

--- models.py
+++ models.py
@@ -1,3 +1,7 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 class DepthEstimation(nn.Module):
     '''
     Class performs Depth.
@@ -16,6 +20,10 @@ class ObjectDetector(nn.Module):
 
     def __init__(self):
         super(ObjectDetector, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=capacity, kernel_size=4, stride=2, padding=1) # out: c x 14 x 14
+        self.conv2 = nn.Conv2d(in_channels=c, out_channels=capacity*2, kernel_size=4, stride=2, padding=1) # out: c x 7 x 7
+        self.fc_mu = nn.Linear(in_features=capacity*2*7*7, out_features=latent_dims)
+        self.fc_logvar = nn.Linear(in_features=capacity*2*7*7, out_features=latent_dims)
 
     def forward(self, images):
 
